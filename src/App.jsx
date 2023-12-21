f28eb82d99c48d8ecf978e7f3b2a55ae1c5c6743
Sudipto das <sudiptodas877@gmail.com>
2023-12-21 08:37:29 +0530

    replace browserRouter with hashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import CandidateDetails from './components/Landingpage'
 import CalculateBmi from './components/CalculateBMI'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   
 
   return (
    <>
-     <Router>
+     <HashRouter>
       <Routes>
         <Route path='/' element={<CandidateDetails/>}/>
         <Route path='/calculateBmi' element={<CalculateBmi/>}/>
       </Routes>
-    </Router>
+    </HashRouter>
    </>
   )
 }
